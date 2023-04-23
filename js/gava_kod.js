7d73f0dd217da56b7025df977949522672d25029
LiudmylaMazur <mazur.lyuda444@gmail.com>
2023-04-23 19:31:00 -0400

    1

---
+++ js/gava_kod.js
@@ -0,0 +1,17 @@
+1;
+// const productName = "Droid";
+// console.log(productName);
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+
+2;
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// // Change code below this line
+// productName = "Repair droid";
+// pricePerItem += 1500;
+// console.log(productName);
+// console.log(pricePerItem);
+
+3;
